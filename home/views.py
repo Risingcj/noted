04e90ad0b2146b089eb2412b56f315d8a32783ff
Chinazom Ezeigbo <126121695+Risingcj@users.noreply.github.com>
2024-06-28 09:14:19 +0100

    first commit

--- home/views.py
+++ home/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from django.contrib import messages
 # Create your views here.
+
+
 def index(request):
     return render(request,"index.html")
     # return HttpResponse("Hello Django App")
+
 
 def contact(request):
     if request.method == 'POST':
